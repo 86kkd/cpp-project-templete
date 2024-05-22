92729a41335318ce14922673a451a3803e1023cf
86kkd <2973720373@qq.com>
2024-05-22 12:13:06 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main(){
+    printf("hello world\n");
+    printf("yes\n");
+    std::cout<<"hello\n";
+}
